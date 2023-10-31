4107a0879a03e7510b40e2dd51de8dc77e0f7866
Lily-Heather Crawford <lilithalicecrawford@gmail.com>
2023-10-31 10:27:18 -0500

    Fix includes

--- include/Game.hpp
+++ include/Game.hpp
@@ -7,12 +7,8 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL2_gfxPrimitives.h>
 
-#include <iostream>
 #include <sstream>
-#include <cstdlib>
 
 
 namespace LCode
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "Game.hpp"
 
+#include <iostream>
+
 int main()
 {
     std::cout << "Hello!\n";
